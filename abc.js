17213d192ef952581352f9fff6aacd2cf0f9d8f3
Khoa Pham <khoaphp@yahoo.com>
2016-11-05 04:05:45 -0700

    start

---
+++ abc.js
@@ -0,0 +1,5 @@
+var mang = ["a", "b", "c"];
+
+mang.forEach(function(element, index, x){
+  console.log(index + ' ' + element + ' ' + x);
+});
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require ('express');
 var app = express();
+var bodyParser = require('body-parser');
+var parser = bodyParser.urlencoded({extended: false});
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.set('views','./views')
@@ -7,4 +9,14 @@ app.listen(3000);
 
 app.get("/", function(req, res){
   res.render("trangchu");
+});
+
+app.get("/info", function(req, res){
+  res.send('KhoaPham Training');
+});
+
+app.post("/abcd", parser, function(req, res){
+  var ten = req.body.ten;
+  var tuoi = req.body.tuoi;
+  res.send(ten + ' ' + tuoi);
 });
